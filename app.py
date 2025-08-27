05e765a49ff52782749cf38e47d81b938de3dc20
Pablo Alcaraz <pabloalcaraz@MacBook-Air-de-Pablo.local>
2025-08-27 13:45:51 -0600

    Initial commit of Psalm Now Flask app

--- app.py
+++ app.py
@@ -137,4 +137,4 @@ def index():
     return render_template('index.html', saludos=saludos, etiqueta=etiqueta)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(debug=True)
